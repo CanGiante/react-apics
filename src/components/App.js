345585198c796c18fda1d7fad55ea8c17323bb53
Valerio D'Ambrosio <63304501+CanGiante@users.noreply.github.com>
2021-04-16 14:24:35 +0200

    app works

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+
+  return (
+    <div 
+      className="app ui container"
+      style={{marginTop:'16px'}}
+    >
+      
+      <SearchBar/>
+
+      app
+
+
+    </div>
+  );
+  
+
+}
+export default App;
