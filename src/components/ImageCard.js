b24f97ce4e227d3becd1a9ae86d780eaf8c158c6
Valerio D'Ambrosio <63304501+CanGiante@users.noreply.github.com>
2021-04-23 16:14:31 +0200

    accessing image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+    this.imageRef = React.createRef();
+  }
+
+
+  componentDidMount() {
+    console.log(this.imageRef);
+    console.log(this.imageRef.current.clientHeight);
+  }
 
   render() {
     const { description, urls } = this.props.image;
@@ -12,6 +24,7 @@ class ImageCard extends React.Component {
         <img 
           src={urls.regular} 
           alt={description}
+          ref={this.imageRef}
         /> 
       </div>
     )
