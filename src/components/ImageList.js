82cb84c743dbd57f9a4db486efa2aada444fa271
Valerio D'Ambrosio <63304501+CanGiante@users.noreply.github.com>
2021-04-23 15:09:04 +0200

    building list of records

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,14 +3,14 @@ import React from 'react';
 const ImageList = (props) => {
 
 
-  const images = props.images.map(image => <img src={image.urls.regular} alt='unsplash picture'/>);
-
-
-  return (
-
-    <div>{images}</div>
-    
+  const images = props.images.map(image => 
+    <div key={image.id}> 
+      <img src={image.urls.regular} alt={image.description}/> 
+    </div>
   );
+
+
+  return <div>{images}</div>
 };
 
 export default ImageList;
