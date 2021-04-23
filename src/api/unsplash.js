c0700bcc02125d662ceabc53ccb71106dbbabf60
Valerio D'Ambrosio <63304501+CanGiante@users.noreply.github.com>
2021-04-23 18:05:57 +0200

    hidden api key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
+const apikey = process.env.REACT_APP_API_AUTH;
 
 export default axios.create({
 
   baseURL: 'https://api.unsplash.com',
 
-  headers: {
-    Authorization: 'Client-ID ***'
-  }
+  headers: { Authorization: 'Client-ID ' + apikey }
+
 });
